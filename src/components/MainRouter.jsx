c89e7a98a53ec7bd10bc7e0117d80e195bdcceb7
LuanPham <luanpham.997@gmail.com>
2018-05-21 21:18:09 +0700

    add some route

--- src/components/MainRouter.jsx
+++ src/components/MainRouter.jsx
@@ -4,7 +4,14 @@ import React from "react";
 import BookByCategory from "./BookByCategory";
 import BookByPublisher from "./BookByPublisher";
 import BookDetail from "./BookDetail";
-import AccountRegister from "./AccountRegister"
+import AccountRegister from "./AccountRegister";
+
+import ContentAdmin from "../admin/Content";
+import ListBook from "../admin/ListBook";
+import ListBookType from "../admin/ListBookType";
+import ListPublisher from "../admin/ListPublisher";
+import ListAccount from "../admin/ListAccount";
+
 const MainRouter = () =>(
     <Switch>
         <Route exact path='/' component={Content}/>
@@ -12,6 +19,12 @@ const MainRouter = () =>(
         <Route path='/AccountRegister' component={AccountRegister}/>
         <Route path='/publisher/:id' component={BookByPublisher}/>
         <Route path='/product/:bookid' component={BookDetail}/>
+        
+        <Route path='/admin' component={ContentAdmin}/>
+        <Route path='/admin/ListBook' component={ListBook}/>
+        <Route path='/admin/ListBookType' component={ListBookType}/>
+        <Route path='/admin/ListPublisher' component={ListPublisher}/>
+        <Route path='/admin/ListAccount' component={ListAccount}/>
     </Switch>
 );
 
