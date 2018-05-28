b3de81c9195fe60fcddbb5067e1a201ee487ac51
huunhuan2002 <huunhuan2002@gmail.com>
2018-05-28 22:01:38 +0700

    tesst

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from "./components/Main.jsx";
+import Main from "./components/Main";
 import {BrowserRouter} from 'react-router-dom';
 ReactDOM.render((
     <BrowserRouter>
