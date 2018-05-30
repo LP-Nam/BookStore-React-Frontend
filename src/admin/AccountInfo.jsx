000e6c92b42b7a6c1064f62a18429dae5902ad64
LuanPham <luanpham.997@gmail.com>
2018-05-30 09:26:17 +0700

    add link trang khach hang

--- src/admin/AccountInfo.jsx
+++ src/admin/AccountInfo.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
 class AccountInfo extends React.Component{
     render()
     {
         return(
             <ul className="nav navbar-nav pull-right">
                 <li><a href="#">Tên tài khoản</a></li>
-                <li><a href="#"> Trang khách hàng</a></li>
+                <li><Link to="/"> Trang khách hàng</Link></li>
                 <li><a href="#"> Đăng xuất</a></li>
             </ul>
         )
